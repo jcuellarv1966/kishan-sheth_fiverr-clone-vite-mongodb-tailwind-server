e13e864d5c0b3ec94c40484c90cae2f8f31edee3
jcuellarv1966 <96638232+jcuellarv1966@users.noreply.github.com>
2024-06-20 12:28:08 -0500

    Initialize server, add dependencies ... and add .env file ...

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+dotenv.config();
+mongoose.set("strictQuery", true);
+
+const connectMongodb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected Successfully!");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+app.use(cors({}));
+app.options("*", cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!🤔";
+
+  return res.status(errorStatus).send(errorMessage);
+});
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
